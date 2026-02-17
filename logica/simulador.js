838a079e9a3c1f555621575b640cf38f1021e7d6
sgarcia24 <sergio_garcia2@intuit.com>
2026-02-18 00:17:27 +0100

    function partidoAleatorio update

--- logica/simulador.js
+++ logica/simulador.js
@@ -1,6 +1,7 @@
 /*
 Importamos el modulo fs de Node 
 */
+const { log } = require("console");
 const fs = require("fs");
 const { type } = require("os");
 /*
@@ -15,33 +16,38 @@ Parsear JSON --> convertir el texto del archivo en objetos JS manipulables
 const archivoEquipos = path.join(__dirname, "../datos/equipos.json");
 const { equipos } = JSON.parse(fs.readFileSync(archivoEquipos, "utf-8"));
 
-// const equipos = objeto.equipos
-
-// console.log(typeof(equipos));
-
-// console.log(equipos[0]);
-
-let equipoBarça = equipos[1];
-let plantillaBarça = equipoBarça.plantilla;
-console.log(typeof(plantillaBarça))
-console.log(plantillaBarça[2].media);
 
 
-let home = Math.floor(Math.random() * equipos.length);
-let guest = Math.floor(Math.random() * equipos.length);
+//Selecciona el indice del equipo local y lo guardamos en una varible:
+let indiceLocal = Math.floor(Math.random() * equipos.length);
 
-function match(home, guest) {
-  if (home !== guest) {
-    console.log(`${home} * ${guest} = ${home * guest}`);
-  } else {
-    console.log(home);
-    console.log(guest);
-    guest = Math.floor(Math.random() * equipos.length);
-    console.log(`${home} + ${guest} = ${home + guest}`);
-  };
+//Selecciona el indice del equipo visitante y lo guardamos en una varible:
+let indiceVisitante = Math.floor(Math.random() * equipos.length);
+
+//Evitamos que los indices de las varibles local/visitante sean iguales y se enfrente entre si el mismo equipo:
+while (indiceLocal === indiceVisitante) {
+  indiceVisitante = Math.floor(Math.random() * equipos.length);
 };
 
-match(home, guest);
+//Una vez obtenidos los indices aleatorios los guardamos como objetos en != variables:
+let equipoLocal = equipos[indiceLocal];
+let equipoVisitante = equipos[indiceVisitante];
+
+
+//funcion que recibe dos parametros (local, visitante) para simular el partido:
+function partidoAleatorio(local, visitante) {
+  let factor = 20;
+  let fuerzaLocal = local.ataque + local.defensa
+  let golesLocal = Math.floor(Math.random() * (fuerzaLocal/factor))
+  let fuerzaVisitante = visitante.ataque + visitante.defensa
+  let golesVisitante = Math.floor(Math.random() * (fuerzaVisitante/factor))
+  let resultado = (`Resultado: ${local.equipo} ${golesLocal} - ${golesVisitante} ${visitante.equipo}`);
+  return resultado;
+};
+console.log(partidoAleatorio(equipoLocal, equipoVisitante));
+// console.log(partidoAleatorio(equipos[0], equipos[3]));
+// let newTeam = {equipo: 'Sevilla', ataque: 79, defensa: 75};
+// console.log(partidoAleatorio(newTeam, equipos[1]));
 
 
 
